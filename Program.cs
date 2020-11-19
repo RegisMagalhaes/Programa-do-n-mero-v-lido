97c75f0a589d9b1be119fda676730804e0e26aae
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-19 01:41:22 -0300

    Este programa solicita um numero valido dentro de um determinado intervalo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace numero_valido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            do {
+                Console.Write("Digite uma nota entre 0 e 10 ");
+                nota = int.Parse(Console.ReadLine());
+
+                if(nota < 0 || nota > 10) {
+                    Console.WriteLine("Numero inválido");
+                }
+            }while(nota < 0 || nota > 10);
+
+            Console.WriteLine("Número aceito");
+        }
+    }
+}
